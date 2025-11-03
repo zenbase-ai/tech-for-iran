dbd456930ffc1bb8b1dd1e7c459d71c291cd324f
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2025-11-04 00:49:44 +0100

    add management commands

---
+++ src/convex/management.ts
@@ -0,0 +1,37 @@
+import { v } from "convex/values"
+import { getManyFrom } from "convex-helpers/server/relationships"
+import { internal } from "@/convex/_generated/api"
+import { internalAction, internalMutation } from "@/convex/_generated/server"
+import { pmap } from "./helpers/collections"
+
+export const dropPod = internalMutation({
+  args: {
+    podId: v.id("pods"),
+  },
+  handler: async (ctx, args) => {
+    const memberships = await getManyFrom(ctx.db, "memberships", "byPod", args.podId, "podId")
+    await pmap(memberships, async (m) => await ctx.db.delete(m._id))
+    await ctx.db.delete(args.podId)
+  },
+})
+
+export const deleteUser = internalAction({
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.runMutation(internal.management.deleteUserMemberships, args)
+    const { account } = await ctx.runMutation(internal.linkedin.deleteAccount, args)
+    await ctx.runAction(internal.linkedin.deleteUnipileAccount, { unipileId: account.unipileId })
+  },
+})
+
+export const deleteUserMemberships = internalMutation({
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const memberships = await getManyFrom(ctx.db, "memberships", "byUser", args.userId, "userId")
+    await pmap(memberships, async (m) => await ctx.db.delete(m._id))
+  },
+})

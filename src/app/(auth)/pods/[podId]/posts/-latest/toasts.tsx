542f45a60126793ad8b4d8c12c2ff2600b4003b4
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2025-11-10 23:41:44 +0000

    refine toasts

--- src/app/(auth)/pods/[podId]/posts/-latest/toasts.tsx
+++ src/app/(auth)/pods/[podId]/posts/-latest/toasts.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { useEffect } from "react"
+import { LuExternalLink } from "react-icons/lu"
 import { toast } from "sonner"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Item, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
+import { Button } from "@/components/ui/button"
+import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia } from "@/components/ui/item"
 import { api } from "@/convex/_generated/api"
 import type { Doc, Id } from "@/convex/_generated/dataModel"
 import useAuthQuery from "@/hooks/use-auth-query"
@@ -30,23 +32,30 @@ export const PostToast: React.FC<PostToastProps> = ({ profile, url, _creationTim
   // biome-ignore lint/correctness/useExhaustiveDependencies: we only want this to run when the posts change
   useEffect(() => {
     toast(
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <Item className="p-0">
-          <ItemMedia>
-            <Avatar>
-              <AvatarImage src={profile.picture} alt={`${profile.firstName} ${profile.lastName}`} />
-              <AvatarFallback className="text-sm font-semibold text-muted-foreground">
-                {profile.firstName[0]}
-                {profile.lastName[0]}
-              </AvatarFallback>
-            </Avatar>
-          </ItemMedia>
-          <ItemContent className="gap-0">
-            <ItemTitle>{profile.firstName}</ItemTitle>
-            <ItemDescription>Posted {timeAgo.format(new Date(_creationTime))}</ItemDescription>
-          </ItemContent>
-        </Item>
-      </a>,
+      <Item className="p-0">
+        <ItemMedia className="-mt-0.5">
+          <Avatar className="size-6">
+            <AvatarImage src={profile.picture} alt={`${profile.firstName} ${profile.lastName}`} />
+            <AvatarFallback className="text-sm font-semibold text-muted-foreground">
+              {profile.firstName[0]}
+              {profile.lastName[0]}
+            </AvatarFallback>
+          </Avatar>
+        </ItemMedia>
+        <ItemContent className="gap-0">
+          <ItemDescription className="font-medium">
+            <span className="text-foreground">{profile.firstName}</span>&nbsp;posted&nbsp;
+            {timeAgo.format(new Date(_creationTime))}
+          </ItemDescription>
+        </ItemContent>
+        <ItemActions>
+          <Button size="icon" variant="ghost" className="size-6" asChild>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              <LuExternalLink className="size-3" />
+            </a>
+          </Button>
+        </ItemActions>
+      </Item>,
       { position: "bottom-left", duration: 10_000 },
     )
   }, [JSON.stringify({ profile, url, _creationTime })])
--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -14,11 +14,11 @@ export const Toaster: React.FC<ToasterProps> = ({ className, ...props }) => {
       theme={theme as ToasterProps["theme"]}
       className={cn("toaster group", className)}
       icons={{
-        success: <LuCircleCheck className="size-5" />,
-        info: <LuInfo className="size-5" />,
-        warning: <LuTriangleAlert className="size-5" />,
-        error: <LuOctagonX className="size-5" />,
-        loading: <Spinner variant="ellipsis" className="size-5" />,
+        success: <LuCircleCheck />,
+        info: <LuInfo />,
+        warning: <LuTriangleAlert />,
+        error: <LuOctagonX />,
+        loading: <Spinner variant="circle" />,
       }}
       style={css({
         "--normal-bg": "var(--popover)",
@@ -29,13 +29,13 @@ export const Toaster: React.FC<ToasterProps> = ({ className, ...props }) => {
       toastOptions={{
         unstyled: true,
         className:
-          "py-3 px-6 rounded-full flex flex-row items-center gap-4 bg-muted/80 backdrop-blur-md border-1 border-muted",
+          "p-3 rounded-full flex flex-row items-center gap-2 bg-muted/80 backdrop-blur-md border-1 border-muted",
         classNames: {
-          icon: "size-5",
+          icon: "size-4",
           loader: "!left-[20%]",
           content: "leading-[1.2]",
-          title: "text-base font-medium",
-          description: "text-sm font-normal",
+          title: "text-sm font-medium",
+          description: "text-xs font-medium",
           success: "!bg-green-500 text-white",
           info: "!bg-blue-500 text-white",
           warning: "!bg-yellow-500 text-white",

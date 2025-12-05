d074751f932dd762c48fcdaf94d44d534b478a91
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2025-12-04 18:45:40 -0700

    tooltip

--- src/components/presenters/post/stats.tsx
+++ src/components/presenters/post/stats.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import type { IconBaseProps } from "react-icons/lib"
-import { LuEye, LuMessageCircle, LuRepeat, LuThumbsUp } from "react-icons/lu"
+import { LuArrowRight, LuEye, LuMessageCircle, LuRepeat, LuThumbsUp } from "react-icons/lu"
+import { useMediaQuery } from "usehooks-ts"
 import { Stack, type StackProps } from "@/components/layout/stack"
 import { Badge, type BadgeProps } from "@/components/ui/badge"
 import { NumberTicker } from "@/components/ui/number-ticker"
@@ -10,6 +11,7 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip
 import { api } from "@/convex/_generated/api"
 import type { Doc, Id } from "@/convex/_generated/dataModel"
 import useAuthQuery from "@/hooks/use-auth-query"
+import { screens } from "@/lib/tailwind"
 import { cn, pluralize } from "@/lib/utils"
 
 export type PostStatsStackProps = StackProps & {
@@ -25,6 +27,7 @@ export const PostStatsStack: React.FC<PostStatsStackProps> = ({
 }) => {
   const stats = useAuthQuery(api.posts.query.stats, { podId, postId })
   const engagementCount = useAuthQuery(api.posts.query.engagementCount, { podId, postId })
+  const sm = useMediaQuery(`(min-width: ${screens.sm})`)
 
   if (stats == null) {
     return <Skeleton className={cn("w-full h-14", className)} />
@@ -36,19 +39,19 @@ export const PostStatsStack: React.FC<PostStatsStackProps> = ({
   }
 
   return (
-    <Stack className={cn("gap-1", className)} items="center" wrap {...props}>
-      <Tooltip>
-        <TooltipTrigger asChild>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Stack className={cn("w-fit gap-1", className)} items="center" wrap {...props}>
           <PostStatBadge field="reactionCount" first={first} last={last} />
-        </TooltipTrigger>
-        <TooltipContent>
-          {pluralize(engagementCount ?? 0, "engagement")} from Crackedbook
-        </TooltipContent>
-      </Tooltip>
-      <PostStatBadge field="commentCount" first={first} last={last} />
-      <PostStatBadge field="repostCount" first={first} last={last} />
-      <PostStatBadge field="impressionCount" first={first} last={last} />
-    </Stack>
+          <PostStatBadge field="commentCount" first={first} last={last} />
+          <PostStatBadge field="repostCount" first={first} last={last} />
+          <PostStatBadge field="impressionCount" first={first} last={last} />
+        </Stack>
+      </TooltipTrigger>
+      <TooltipContent side={sm ? "right" : "bottom"}>
+        {pluralize(engagementCount ?? 0, "engagement")} from Crackedbook
+      </TooltipContent>
+    </Tooltip>
   )
 }
 
@@ -58,20 +61,33 @@ export type PostStatBadgeProps = BadgeProps & {
   last: Doc<"stats">
 }
 
-export const PostStatBadge: React.FC<PostStatBadgeProps> = ({ field, first, last, ...props }) =>
-  last[field] === 0 ? null : (
-    <Badge size="sm" variant="ghost" {...props}>
-      <span className="inline-flex gap-0.5">
-        <span className="text-muted-foreground">{first[field]}</span>
-        {first._id !== last._id && (
+export const PostStatBadge: React.FC<PostStatBadgeProps> = ({
+  field,
+  first,
+  last,
+  className,
+  ...props
+}) => {
+  if (last[field] === 0) {
+    return null
+  }
+
+  const hasHistory = first._id !== last._id
+  return (
+    <Badge className={cn("text-muted-foreground", className)} size="sm" variant="ghost" {...props}>
+      <span className="inline-flex gap-0.5 items-center">
+        {first[field]}
+        {hasHistory && (
           <>
-            +<NumberTicker value={last[field] - first[field]} />
+            <LuArrowRight className="size-3" />
+            <NumberTicker className="text-foreground" value={last[field]} />
           </>
         )}
       </span>
-      <PostStatIcon className="text-muted-foreground" field={field} />
+      <PostStatIcon field={field} />
     </Badge>
   )
+}
 
 export type PostStatIcon = IconBaseProps & Pick<PostStatBadgeProps, "field">
 

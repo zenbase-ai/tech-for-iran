518f94d7bdb74fe01d4c386397fa6dec85665ff2
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2025-12-08 11:40:08 -0700

    mx-auto on clerk layout

--- src/app/(clerk)/layout.tsx
+++ src/app/(clerk)/layout.tsx
@@ -2,7 +2,7 @@ import { Box } from "@/components/layout/box"
 
 export default function ClerkLayout({ children }: React.PropsWithChildren) {
   return (
-    <Box as="main" className="min-h-[60vh]">
+    <Box as="main" className="min-h-[60vh] mx-auto">
       {children}
     </Box>
   )

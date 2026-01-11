7b97bd1120e8de161665c50be053e4cb7e156311
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2026-01-11 14:31:19 -0700

    better nav

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -5,11 +5,13 @@ import { motion } from "motion/react"
 import { LuPenLine, LuThumbsUp } from "react-icons/lu"
 import { Logo } from "@/components/assets/logo"
 import { HStack } from "@/components/layout/stack"
+import { Button } from "@/components/ui/button"
 import { GlowEffect } from "@/components/ui/glow-effect"
 import { NumberTicker } from "@/components/ui/number-ticker"
 import { ThemeToggler } from "@/components/ui/theme-toggler"
 import { api } from "@/convex/_generated/api"
-import { cn } from "@/lib/utils"
+import { cn, xProfileURL } from "@/lib/utils"
+import { Badge } from "../ui/badge"
 
 export const Nav: React.FC = () => {
   const signatureCount = useQuery(api.signatures.query.count)
@@ -20,31 +22,36 @@ export const Nav: React.FC = () => {
       <motion.nav
         animate={{ opacity: 1, y: 0, x: 0 }}
         className={cn(
-          "fixed z-42 flex flex-row gap-3 items-center justify-between",
+          "fixed z-42 flex flex-row gap-8 items-center justify-between",
           "left-4 lg:left-8 xl:left-16 top-4 lg:top-8 xl:top-16 z-42"
         )}
         initial={{ opacity: 0, y: -16 }}
       >
         <HStack
           className={cn(
-            "gap-5 px-4 h-10",
+            "px-0.5 h-10",
             "rounded-full",
             "bg-background/50 backdrop-blur-md",
             "border shadow-md",
             "border-border",
-            "text-sm font-medium text-muted-foreground"
+            "text-base font-medium text-muted-foreground"
           )}
           items="center"
         >
-          <HStack className="gap-2 text-lg" items="center">
+          <ThemeToggler variant="ghost" />
+          <Button asChild variant="ghost">
+            <a href={xProfileURL("cyrusnewday")} rel="noopener noreferrer" target="_blank">
+              Made by Cyrus
+            </a>
+          </Button>
+          <Badge variant="ghost">
             <NumberTicker className="tabular-nums" value={signatureCount ?? 0} />
-            <LuPenLine className="size-4.5" />
-          </HStack>
-          <HStack className="gap-2 text-lg" items="center">
+            <LuPenLine className="size-4" />
+          </Badge>
+          <Badge variant="ghost">
             <NumberTicker className="tabular-nums" value={upvoteCount ?? 0} />
-            <LuThumbsUp className="size-4.5" />
-          </HStack>
-          <ThemeToggler className="-mr-3.5" variant="ghost" />
+            <LuThumbsUp className="size-4" />
+          </Badge>
         </HStack>
       </motion.nav>
       <motion.div

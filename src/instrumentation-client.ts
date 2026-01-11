2b22a4fdbea9626a56af1e023881c6129d1f55e8
Cyrus <1880797+CyrusNuevoDia@users.noreply.github.com>
2026-01-11 00:01:50 -0700

    add posthog

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,8 +1,8 @@
-// import posthog from "posthog-js"
-// import { env } from "@/lib/env.mjs"
+import posthog from "posthog-js"
+import { env } from "@/lib/env.mjs"
 
-// posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-//   api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-//   ui_host: "https://us.i.posthog.com",
-//   defaults: "2025-05-24",
-// })
+posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+  api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+  ui_host: "https://us.i.posthog.com",
+  defaults: "2025-05-24",
+})
